daa93f98f786e3b3464b671284582c15fe7664a5
gevellyn <145408706+gevellyn@users.noreply.github.com>
2023-10-20 19:56:14 -0300

    js cont

--- aluramidi-evellynp/main.js
+++ aluramidi-evellynp/main.js
@@ -1,2 +1,7 @@
-function tocaSomPom () {
-document.querySelector(s)
+function tocaSom (idELementoAudio) {
+document.querySelector(idELementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll ('.tecla');
+
+for (let contador =0;contador < ListaDeTeclas.length; contador++) 
