ab2b54dabe9dd6c5059dfc7852bbb985bdccadce
gevellyn <145408706+gevellyn@users.noreply.github.com>
2023-10-20 20:47:44 -0300

    js cont2

--- aluramidi-evellynp/main.js
+++ aluramidi-evellynp/main.js
@@ -4,4 +4,10 @@ document.querySelector(idELementoAudio).play();
 
 const ListaDeTeclas = document.querySelectorAll ('.tecla');
 
-for (let contador =0;contador < ListaDeTeclas.length; contador++) 
+for (let contador =0;contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+const idAudio = '#som_${instrumento}';
+
+tecla.onclick = function () 
