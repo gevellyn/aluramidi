cef818f625de2efadc16fd76d80f3d2d4738c2ae
gevellyn <145408706+gevellyn@users.noreply.github.com>
2023-10-26 21:37:03 -0300

    jsatualizado

--- aluramidi-evellynp/main.js
+++ aluramidi-evellynp/main.js
@@ -8,7 +8,7 @@ for (let contador =0;contador < ListaDeTeclas.length; contador++) {
 
     const tecla = ListaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-const idAudio = '#som_${instrumento}';
+const idAudio = `#som_${instrumento}`;
 
 tecla.onclick = function () {
     tocaSom(idAudio);
